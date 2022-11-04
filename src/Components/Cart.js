5815838dded214e928f91a096904f474afd02380
Ganesh Kaarthik <ganeshkaarthikmessi@gmail.com>
2022-11-04 23:58:50 +0530

    added changes

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -5,13 +5,13 @@ const Cart = ({ count, setCount, cartButton }) => {
 
   return (
     <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-      <div className="text-center">
-        {cartButton === "Add to cart" ?
-          (<a href="#" className="btn btn-outline-dark mt-auto"
-            onClick={() => { setCount(add ? count - 1 : count + 1); setAdd(!add) }}>
-            {add ? "Remove from " : " Add to "} cart </a>) :
-          (<a href="#" className="btn btn-outline-dark mt-auto">View Options</a>)}
-      </div>
+        <div className="text-center">
+            {cartButton === "Add to cart" ?
+            (<a href="#" className="btn btn-outline-dark mt-auto"
+              onClick={() => { setCount(add ? count - 1 : count + 1); setAdd(!add) }}>
+              {add ? "Remove from " : " Add to "} cart </a>) :
+            (<a href="#" className="btn btn-outline-dark mt-auto">View Options</a>)}
+        </div>
     </div>
   );
 }
--- src/Components/ProductReviews.js
+++ src/Components/ProductReviews.js
@@ -3,14 +3,14 @@ import React from 'react';
 const ProductReviews = ({ reviews }) => {
   return (
     <div>
-      {reviews ? (
-        <div class="d-flex justify-content-center small text-warning mb-2">
-          <div class="bi-star-fill"></div>
-          <div class="bi-star-fill"></div>
-          <div class="bi-star-fill"></div>
-          <div class="bi-star-fill"></div>
-          <div class="bi-star-fill"></div>
-        </div>
+        {reviews ? (
+          <div class="d-flex justify-content-center small text-warning mb-2">
+            <div class="bi-star-fill"></div>
+            <div class="bi-star-fill"></div>
+            <div class="bi-star-fill"></div>
+            <div class="bi-star-fill"></div>
+            <div class="bi-star-fill"></div>
+          </div>
       ) : (
         <div></div>
       )}
